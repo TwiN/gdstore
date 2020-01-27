0259f6bdf15123af2745f594f03be7ee2bcd4f52
TwinProduction <twin@twinnation.org>
2020-01-26 19:42:25 -0500

    Add private saveToDisk and loadFromDisk functions + in-memory map

--- README.md
+++ README.md
@@ -2,10 +2,31 @@
 
 **gdstore**, short for **G**o **D**isk store, is a key-value library in Go for persisting data to disk.
 
-This repository does not have speed as its main purpose, but rather, ease of use.
+This library does not have speed as its main purpose, but rather, ease of use.
+
+If you're looking for a high-performance key-value store/database/cache, there are definitely better
+alternatives, but if you're searching for a simple way to persist key-value entries to disk, then 
+this is definitely what you're looking for.
 
 
+## Write
+
+```go
+
+```
 
 
+## Read
+
+```go
+
+```
+
+
+## Delete
+
+```go
+
+```
 
 
--- main.go
+++ main.go
@@ -8,26 +8,43 @@ var (
 
 type GDStore struct {
 	FilePath string
+	data     map[string][]byte
 }
 
 func New(filePath string) *GDStore {
-	return &GDStore{
+	store := &GDStore{
 		FilePath: filePath,
+		data:     make(map[string][]byte),
 	}
+	store.loadFromDisk()
+	return store
 }
 
-func (store GDStore) Get(key string) ([]byte, error) {
+func (store *GDStore) Get(key string) ([]byte, error) {
+	if value, ok := store.data[key]; ok {
+		return value, nil
+	}
 	return nil, ErrNotFound
 }
 
-func (store GDStore) Put(key string, value []byte) error {
+func (store *GDStore) Put(key string, value []byte) error {
+	store.data[key] = value
+	return store.saveToDisk()
+}
+
+func (store *GDStore) Delete(key string) error {
+	delete(store.data, key)
+	return store.saveToDisk()
+}
+
+func (store *GDStore) Count() int {
+	return len(store.data)
+}
+
+func (store *GDStore) saveToDisk() error {
 	return nil
 }
 
-func (store GDStore) Delete(key string) error {
-	return nil
-}
+func (store *GDStore) loadFromDisk() {
 
-func (store GDStore) Count() int {
-	return 0
 }
